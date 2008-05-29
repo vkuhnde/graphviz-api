ecfab99503b68b47ecb0c9e9bc654f66251425a0
Kohsuke Kawaguchi <kk@kohsuke.org>
2008-05-29 07:43:36 +0000

    added more test code

---
+++ src/test/java/SimpleTest.java
@@ -0,0 +1,18 @@
+import junit.framework.TestCase;
+import static org.kohsuke.graphviz.Attribute.COLOR;
+import org.kohsuke.graphviz.Graph;
+import org.kohsuke.graphviz.Style;
+
+import java.awt.*;
+import java.util.Arrays;
+import java.io.File;
+import java.io.IOException;
+
+public class SimpleTest extends TestCase {
+    public void test1() throws IOException, InterruptedException {
+        Graph graph = new Graph();
+        Style s = new Style();
+        s.attr(COLOR, Color.RED);
+        graph.with(s).node("a").to().node("b").generateTo(Arrays.asList("dot","-Tgif"),new File("test.gif"));
+    }
+}
