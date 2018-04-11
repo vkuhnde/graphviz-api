eafcfbce79e687fa4fc3272d6ed5ce837c7e1c72
Kuhn, Valentin <valentin.kuhn@accso.de>
2018-04-11 11:13:02 +0200

    fix overlap class name

--- src/main/java/org/kohsuke/graphviz/Overlap.java
+++ src/main/java/org/kohsuke/graphviz/Overlap.java
@@ -4,7 +4,7 @@ package org.kohsuke.graphviz;
  * c.f. https://www.graphviz.org/doc/info/attrs.html#d:overlap
  * @author Valentin Kuhn
  */
-public enum Orientation {
+public enum Overlap {
     TRUE, FALSE, SCALE, PRISM, SCALEXY, COMPRESS, VPSC,
     @Deprecated ORTHO, @Deprecated ORTHOXY, @Deprecated ORTHOYX, @Deprecated ORTHO_YX,
     @Deprecated PORTHO, @Deprecated PORTHOXY, @Deprecated PORTHOYX, @Deprecated PORTHO_YX
