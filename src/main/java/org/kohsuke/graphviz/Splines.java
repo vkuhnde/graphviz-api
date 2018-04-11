74d9018342b736da02337cb87cbd97f75d5a95fe
Kuhn, Valentin <valentin.kuhn@accso.de>
2018-04-11 11:20:47 +0200

    add splines attribute

---
+++ src/main/java/org/kohsuke/graphviz/Splines.java
@@ -0,0 +1,9 @@
+package org.kohsuke.graphviz;
+
+/**
+ * c.f. https://www.graphviz.org/doc/info/attrs.html#d:splines
+ * @author Valentin Kuhn
+ */
+public enum Splines {
+    NONE,LINE,FALSE,POLYLINE,CURVED,ORTHO,SPLINE,TRUE
+}
